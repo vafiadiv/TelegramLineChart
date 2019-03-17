5cf371849dace50728c30d9a8e747b50a77eb2d1
vafiadiv <vafiadiv@gmail.com>
2019-03-17 22:32:29 +0300

    Project setup

--- TelegramLineChart/View Controllers/ChartViewController.swift
+++ TelegramLineChart/View Controllers/ChartViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .red
     }
 
 
