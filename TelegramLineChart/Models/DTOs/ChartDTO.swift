bc12f00baa0b44d4adc68c4242e3b91d18cdcfc3
Valentin Vafiadi <valentin.vafiadi@cleverpumpkin.ru>
2019-03-20 02:02:50 +0300

    ChartDTO - columns mapping

--- TelegramLineChart/Models/DTOs/ChartDTO.swift
+++ TelegramLineChart/Models/DTOs/ChartDTO.swift
@@ -8,6 +8,7 @@ import Foundation
 struct ChartDTO: Decodable {
 //	let columns: [[Int]]
 	let types: [String: String]
+	let columns: [String: [Int]]
 
 	private enum CodingKeys: CodingKey {
 		case columns
@@ -19,7 +20,27 @@ struct ChartDTO: Decodable {
 	init(from decoder: Decoder) throws {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
 
-		let dict = try container.decode([String: String].self, forKey: .types)
-		self.types = dict
+		self.types = try container.decode([String: String].self, forKey: .types)
+
+		var columns = [String: [Int]]()
+		self.types.forEach { key, _ in columns[key] = [Int]() }
+
+		var columnsContainer = try container.nestedUnkeyedContainer(forKey: .columns)
+		while !columnsContainer.isAtEnd {
+			var columnContainer = try columnsContainer.nestedUnkeyedContainer()
+			var index = 0
+			var columnType = ""
+			while !columnContainer.isAtEnd {
+				if index == 0 {
+					columnType = try columnContainer.decode(String.self)
+				} else {
+					let rowValue = try columnContainer.decode(Int.self)
+					columns[columnType]?.append(rowValue)
+				}
+				index += 1
+			}
+		}
+
+		self.columns = columns
 	}
 }
